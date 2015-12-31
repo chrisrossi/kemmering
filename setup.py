89abadfc82bb55876aa84409ee97da91e9d11da7
Chris Rossi <chris@archimedeanco.com>
2015-12-31 16:42:02 -0500

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+from setuptools import find_packages
+
+VERSION = '0.1dev'
+
+requires = [
+]
+tests_require = requires + []
+
+setup(name='kemmer',
+      version=VERSION,
+      description='Super simple XML tags',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      )
